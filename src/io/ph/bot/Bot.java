f7c6eca027408e4f3d99417ba299956b90ccf897
nickalaskreynolds <nickalaskreynolds@gmail.com>
2018-08-17 23:30:28 +0000

    fixing version #

--- src/io/ph/bot/Bot.java
+++ src/io/ph/bot/Bot.java
@@ -55,7 +55,7 @@ public class Bot {
 
     // Set to true if you want various debug statements
     public static final boolean DEBUG = false;
-    public static final String BOT_VERSION = "1.0";
+    public static final String BOT_VERSION = "1.0.1.1";
     public static final String REPO = "<https://github.com/nickalaskreynolds/Star-Bot>";
     public static boolean isReady = false;
 
