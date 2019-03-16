f1bb6426fd1401e2a9d782c5c9be413815c7da46
Nickalas Reynolds <nickalaskreynolds@gmail.com>
2019-03-15 21:37:25 -0600

    removed all files

--- src/io/ph/bot/audio/stream/StreamSource.java
+++
@@ -1,6 +0,0 @@
-package io.ph.bot.audio.stream;
-
-public enum StreamSource {
-    LISTEN_MOE,
-    YOUTUBE;
-}
--- src/io/ph/bot/events/CustomEventDispatcher.java
+++
@@ -1,7 +0,0 @@
-package io.ph.bot.events;
-
-public class CustomEventDispatcher {
-    public void dispatch(BotEvent e) {
-        e.handle();
-    }
-}
