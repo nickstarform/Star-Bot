ce7bd739fc4e451af6397bba92d72c2991df0dc4
nickalaskreynolds <nickalaskreynolds@gmail.com>
2019-03-20 02:45:44 -0500

    fixed minor errors will nulls;

--- src/io/ph/bot/commands/administration/Welcome.java
+++ src/io/ph/bot/commands/administration/Welcome.java
@@ -6,8 +6,12 @@ import io.ph.bot.commands.CommandData;
 import io.ph.bot.model.GuildObject;
 import io.ph.bot.model.Permission;
 import io.ph.util.Util;
+import io.ph.util.MessageUtils;
 import io.ph.bot.Bot;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.TextChannel;
 import java.lang.StringBuilder;
 
 /**
@@ -20,7 +24,8 @@ import java.lang.StringBuilder;
         aliases = {"welcomemessage"},
         category = CommandCategory.ADMINISTRATION,
         permission = Permission.MANAGE_SERVER,
-        description = "Have the bot demo the welcome message."
+        description = "Have the bot demo the welcome message.",
+	example = ""
         )
 public class Welcome extends Command {
 
@@ -28,32 +33,30 @@ public class Welcome extends Command {
     public void executeCommand(Message msg) {
         Guild guild = msg.getGuild();
         GuildObject g = GuildObject.guildMap.get(guild.getId());
-        String ret = '';
+        String ret;
         StringBuilder stringBuilder = new StringBuilder();
 
         // Welcome message
         if (!g.getConfig().getWelcomeMessage().isEmpty()) {
             // weird instance to get the botinvitelink, split and just get the botidlong
             String botIDlong = Bot.getInstance().getConfig().getBotInviteBotLink().split("\\=")[1].split("\\&")[0];
-            Member bot = Util.resolveMemberFromMessage(botIDlong, guild);
-            TextChannel chanName;
+            Member bot = Util.resolveUserFromMessage(botIDlong, guild.getId());
             if (g.getConfig().isPmWelcomeMessage()) {
                 stringBuilder.append("Will PM the user.\n");
             }
             if (!g.getSpecialChannels().getWelcome().equals("")) {
-                chanName = guild.getTextChannelById(g.getSpecialChannels().getRulesChannel());
-                stringBuilder.append("Will send a message to the channel: " + chanName.getAsMention() + "\n");
+                TextChannel chanName = guild.getTextChannelById(g.getSpecialChannels().getRulesChannel());
+                stringBuilder.append("Will send a message to the channel: <#" + chanName.getId() + ">\n");
             }
 
             ret = g.getConfig().getWelcomeMessage();
-
+            System.out.println("BotID: "+ botIDlong);
             ret = ret.replaceAll("\\$user\\$", bot.getAsMention());
             ret = ret.replaceAll("\\$server\\$", guild.getName());
 
-            if (ret.contains("\\$channel\\$")) {
-                if (!g.getSpecialChannels().getRulesChannel().equals("")) {
-                    ret = ret.replaceAll("\\$channel\\$", chanName.getAsMention());
-                }
+            if (!g.getSpecialChannels().getWelcome().equals("")) {
+                TextChannel chanName = guild.getTextChannelById(g.getSpecialChannels().getRulesChannel());
+                ret = ret.replaceAll("\\$channel\\$", "<#" + chanName.getId() + ">");
             }
         } else {
             ret = "No message has been set, use the `changewelcome` command to set it.";
